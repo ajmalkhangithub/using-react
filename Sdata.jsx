6c121ad32bdb7149ce947961f9be0893bd5b4389
Ajmal khan <144774577+ajmalkhangithub@users.noreply.github.com>
2024-08-03 12:05:23 +0500

    Add files via upload

---
+++ Sdata.jsx
@@ -0,0 +1,39 @@
+const Sdata=[
+    {
+        imgsrc:"https://static1.moviewebimages.com/wordpress/wp-content/uploads/2022/07/Dark.jpg" ,
+        title:"A Netflix original series",
+        sname:"Dark",
+        link:"https://movieweb.com/dark-german-netflix-show-death-and-determinism/"
+    },
+    {
+        imgsrc:"https://static1.moviewebimages.com/wordpress/wp-content/uploads/2022/07/Dark.jpg" ,
+        title:"A Netflix original series",
+        sname:"extraCurricular",
+        link:"https://movieweb.com/dark-german-netflix-show-death-and-determinism/"
+    },
+    {
+        imgsrc:"https://static1.moviewebimages.com/wordpress/wp-content/uploads/2022/07/Dark.jpg" ,
+        title:"A Netflix original series",
+        sname:"Stranger",
+        link:"https://movieweb.com/dark-german-netflix-show-death-and-determinism/"
+    },
+    {
+        imgsrc:"https://static1.moviewebimages.com/wordpress/wp-content/uploads/2022/07/Dark.jpg" ,
+        title:"A Netflix original series",
+        sname:"Stranger",
+        link:"https://movieweb.com/dark-german-netflix-show-death-and-determinism/"
+    },
+    {
+        imgsrc:"https://static1.moviewebimages.com/wordpress/wp-content/uploads/2022/07/Dark.jpg" ,
+        title:"A Netflix original series",
+        sname:"Stranger",
+        link:"https://movieweb.com/dark-german-netflix-show-death-and-determinism/"
+    },
+    {
+        imgsrc:"https://static1.moviewebimages.com/wordpress/wp-content/uploads/2022/07/Dark.jpg" ,
+        title:"A Netflix original series",
+        sname:"Stranger",
+        link:"https://movieweb.com/dark-german-netflix-show-death-and-determinism/"
+    }
+];
+export default Sdata;
